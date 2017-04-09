9fc95a2a560d4b81cb8b49fcd24ae7a770a3e73b
Никита Крамаренко <nk_95@bk.ru>
2017-04-09 13:23:27 +1000

    Add Tower Class

--- Hackaton/Tower.cs
+++ Hackaton/Tower.cs
@@ -1,16 +1,27 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using Microsoft.Xna.Framework;
 
 namespace Hackaton{
-    class Tower {
+    class Tower{
+        public int PosX, PosY, height, width;
+        Texture2D Texture, PressTexture;
+        SpriteBatch spriteBatch;
+        public int Level;
+        public int Damage;
+
+        public Tower(int Ax, int Ay, int Awidth, int Aheigh, SpriteBatch AspriteBatch) {
+            PosX = Ax; PosY = Ay;
+            width = Awidth;
+            height = Aheigh;
+            spriteBatch = AspriteBatch;
+            }
+
+        public void SetImage(string Tpath, string PTpath, ContentManager Content) {
+        }
+
+        public void Draw() {
+            spriteBatch.Draw(isPress ? Texture : PressTexture, new Rectangle(Game1.NormalizeX(PosX), Game1.NormalizeY(PosY),
+                Game1.NormalizeX(width), Game1.NormalizeY(height)), Color.White);
+        }
     }
 }
